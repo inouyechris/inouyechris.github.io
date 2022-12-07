9b1f4dfb3bc519696f005feae459d02405a7d3d6
inouyechris <inouyec916@gmail.com>
2022-12-06 16:49:49 -0800

    added local storage counter to assignment 4

--- fancifymytext.js
+++ fancifymytext.js
@@ -31,4 +31,11 @@ var suffixed_text=split_text.join("_");
 let joined_text=suffixed_text.concat("-Moo")
 
 document.getElementById("textbox").value=joined_text;
+
+if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount)+1;
+  } else {
+    localStorage.clickcount = 1;
+  }
+  document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
 }
